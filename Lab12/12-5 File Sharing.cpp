82ad480a2fa39e3842427e82fa44040a8feb2d77
Duangmani090 <684244106@mail.pbru.ac.th>
2026-03-13 10:13:13 +0700

    Implement reader function with shared locking

--- Lab12/12-5 File Sharing.cpp
+++ Lab12/12-5 File Sharing.cpp
@@ -19,3 +19,24 @@ cout &lt;&lt; &quot;Writer: wrote and released lock&quot; &lt;&lt; endl;
 }
 // (b) READER: shared lock
 void reader(const char* filename) {
+int fd = open(filename, O_RDONLY);
+if (fd == -1) { perror(&quot;open&quot;); return; }
+// LOCK_SH: shared lock (multiple readers allowed simultaneously)
+if (flock(fd, LOCK_SH) == -1) { perror(&quot;flock&quot;); close(fd); return; }
+char buf[256]; ssize_t n;
+cout &lt;&lt; &quot;Reader output:&quot; &lt;&lt; endl;
+while ((n = read(fd, buf, sizeof(buf)-1)) &gt; 0) {
+buf[n] = &#39;\0&#39;;
+cout &lt;&lt; buf;
+}
+flock(fd, LOCK_UN); // Release shared lock
+close(fd);
+}
+int main() {
+writer(&quot;shared.txt&quot;, &quot;Process A data&quot;);
+writer(&quot;shared.txt&quot;, &quot;Process B data&quot;);
+reader(&quot;shared.txt&quot;);
+return 0;
+}
+// LOCK_SH: multiple readers concurrently OK
+// LOCK_EX: single writer; blocks all other readers and writers
